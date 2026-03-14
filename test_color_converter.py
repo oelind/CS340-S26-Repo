938b9a7c799513e00ab7fa08fb0a54cd0f8644e5
Oakley Lindsay <123098548+oelind@users.noreply.github.com>
2026-03-14 18:29:09 -0400

    Update test_color_converter.py
    Modified the test for the color black by adding three zeros so the leading zeros are included in the check for the correct output

--- test_color_converter.py
+++ test_color_converter.py
@@ -6,7 +6,8 @@ class TestColorConverter(unittest.TestCase):
         self.assertEqual(rgb_to_hex(255, 255, 255), "#ffffff")
 
     def test_black(self):
-        self.assertEqual(rgb_to_hex(0, 0, 0), "#000") 
+        #added three 0s to ensure leading zeros are included
+        self.assertEqual(rgb_to_hex(0, 0, 0), "#000000") 
 
 if __name__ == '__main__':
     unittest.main()
